41c7fbe32d8f439797e64a04841d16c74ce19449
Shizue Chalwell <shizue46@gmail.com>
2019-09-26 04:04:50 +0000

    Front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def index
-  @page = Page.order("RANDOM()").first
+    @page = Page.order("RANDOM()").first
   end
+
+  def new 
+    @joke - Joke.new
+  end 
 end
