4d7eee5f0d9490594e8ff2dac8df94499c68674a
Shizue Chalwell <shizue46@gmail.com>
2019-09-27 05:17:12 +0000

    Fin

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -2,4 +2,19 @@ class JokesController < ApplicationController
   def index
     @joke = Joke.order("RANDOM()").first
   end 
+
+  def new 
+    @joke = Joke.new
+  end 
+
+  def create
+    Joke.create(joke_params) 
+    redirect_to root_path
+  end 
+
+  private 
+
+  def joke_params 
+    params.require(:joke).permit(:joke, :punchline, :name)
+  end
 end
